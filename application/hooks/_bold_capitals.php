b9b9d62608eed8c835ff277360ac97758d3f7f56
3rafanan <3rafanan@gmail.com>
2015-01-25 18:31:41 -0800

    Implemented a better version for adding strong tags after deadline
    Signed-off-by: 3rafanan <3rafanan@gmail.com>

--- application/hooks/_bold_capitals.php
+++ application/hooks/_bold_capitals.php
@@ -5,15 +5,30 @@ function bold_capitals()
 	$CI =& get_instance();
 	$buffer = $CI->output->get_output();
 
-    // Find all capital letters between <p class="lead"></p><br/>
-    $buffer = preg_replace_callback("/[A-Z](?=(?:.(?!<p class=\"lead\">))*<\/p><br\/>)/",
-                function ($matches) {
-                    return "<strong>" . $matches[0] . "</strong>"; // Put capital letters inside strong tags
-                },
-                $buffer);
+    // Create DOM object and load the output buffer
+    $dom = new DOMDocument();
+    $dom->loadHTML($buffer);
 
-    $CI->output->set_output($buffer);
+    $xpath = new DOMXpath($dom);
+    // look for strings inside element p with attribute class="lead"
+    $list = $xpath->query('//p[@class="lead"]');
+
+    foreach($list as $node) {
+        // replace the strings with strong capital letters
+        $search = "'" . $node->nodeValue . "'";
+        $replacement = preg_replace('(([A-Z]))', '<strong>$1</strong>', $search);
+        $buffer = preg_replace($search, $replacement, $buffer);
+    }
+
+    // *NOTE: Not sure why but this doesn't work with the /dunno link.
+    //        It doesn't render the <strong> tag properly. Even page
+    //        source does not have a <strong> tag anywhere.
+    //        All other links works properly.
+
+	$CI->output->set_output($buffer);
 	$CI->output->_display();
+
+
 }
 
 /* End of file _bold_capitals.php */
